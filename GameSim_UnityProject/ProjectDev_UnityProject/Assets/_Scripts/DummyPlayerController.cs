2d36d1d49c78e22a4279b2e1e34c5ec2d993c0f9
egatlin1 <egatlin1@student.alamo.edu>
2018-09-14 10:48:00 -0500

    Camera position changes
    Created a "Camera Change Trigger" that moves the camera when the player passes through it.
    
    Added dummy script to moved a box  with A and D to demonstrate triggers.

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/DummyPlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayerController : MonoBehaviour
+{
+
+	// Terrible horrible code, but works for small demo to demostrate camera movement when player moves from screen to screen
+	void Update () {
+        this.transform.position += new Vector3(Input.GetAxis("Horizontal") * 5f * Time.deltaTime, 0);
+    }
+}
