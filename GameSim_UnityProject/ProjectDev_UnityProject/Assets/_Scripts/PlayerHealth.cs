389c41f244276bfdad5ae5fc9a529155d656559f
RickMP <rpena280@student.alamo.edu>
2018-09-18 19:03:01 -0500

    Re-Added Health Script
    Re-made health script and added to player in scene

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	float health;
+
+	public void addHealth(float heal = 1)
+	{
+		health += heal;
+	}
+
+	public void loseHealth(float damage = 1)
+	{
+		health -= damage;
+	}
+}
