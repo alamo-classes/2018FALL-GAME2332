8a16156871744ba8f31414a3a169ac90237dc63c
egatlin1 <egatlin1@student.alamo.edu>
2018-09-19 13:28:39 -0500

    Added health orbs
    Added health orbs that heal the player when picked up
    
    Healing amount is changable and the size of the orbs cahnges depending on how much it heals

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/HealthOrb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthOrb : MonoBehaviour {
+
+    public float healingPoints = 1f;
+
+    private void Start()
+    {
+        this.transform.localScale *= healingPoints;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject obj = other.gameObject;
+        PlayerHealth health = other.GetComponent<PlayerHealth>();
+        if ( health )
+        {
+            health.addHealth(healingPoints);
+            Destroy(gameObject);
+        }
+    }
+
+}
