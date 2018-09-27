079806a635a920df44793bc1cb164f98a5136f53
SAKillen <skillen3@alamo.edu>
2018-09-27 17:36:30 -0500

    Added enemy
    Colliding with enemies causes health loss, though to say they "attack" at this point is...generous.

--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/HealthOrb.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/HealthOrb.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthOrb : MonoBehaviour {
+public class HealthOrb : MonoBehaviour
+{
+   public float healingPoints = 1f;
 
-    public float healingPoints = 1f;
+   private void Start()
+   {
+      this.transform.localScale *= healingPoints;
+   }
 
-    private void Start()
-    {
-        this.transform.localScale *= healingPoints;
-    }
-
-    public float HealingPoints ( )
-    {
-        return healingPoints;
-    }
+   public float HealingPoints()
+   {
+      return healingPoints;
+   }
 }
--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/PlayerHealth.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/PlayerHealth.cs
@@ -2,30 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
 
-	public float health;
+   public float health;
 
-	public void addHealth(float heal = 1)
-	{
-		health += heal;
-	}
+   public void addHealth(float heal = 1)
+   {
+      health += heal;
+   }
 
-	public void loseHealth(float damage = 1)
-	{
-		health -= damage;
-	}
+   public void loseHealth(float damage = 1)
+   {
+      health -= damage;
+   }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        GameObject obj = other.gameObject;
-        HealthOrb healingOrb = other.GetComponent<HealthOrb>();
-        if (healingOrb)
-        {
-            addHealth(healingOrb.HealingPoints());
-            Destroy(other.gameObject);
-        }
-    }
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      GameObject obj = other.gameObject;
+      HealthOrb healingOrb = other.GetComponent<HealthOrb>();
+      EnemyAttack attack = other.GetComponent<EnemyAttack>();
+
+      if (healingOrb)
+      {
+         addHealth(healingOrb.HealingPoints());
+         Destroy(other.gameObject);
+      }
+
+      if (attack)
+      {
+         loseHealth(attack.attackDamage);
+      }
+   }
 
 
 }
