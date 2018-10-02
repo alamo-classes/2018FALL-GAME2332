a85037be4bb12abae1f65e125d17c114352fb737
SAKillen <skillen3@alamo.edu>
2018-10-02 02:10:56 -0500

    Enemy movement now working
    Attempt at enemy patrol functionality under way.

--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/EnemyMovement.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/EnemyMovement.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+   public float speed = 1f;
+   public float detectRange = 10f;
+   bool inRange;
+   Transform player;
+   public Transform[] route;
+   private int routeDest = 0;
 
+   void Start()
+   {
+      player = GameObject.FindGameObjectWithTag("Player").transform;
+      Patrol();
+   }
+
+   void Update()
+   {
+      float detectDistance = (player.position - transform.position).sqrMagnitude;
+
+      if (detectDistance <= detectRange)
+         inRange = true;
+      else
+         inRange = false;
+
+      if (inRange)
+         transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+      else
+         Patrol();
+   }
+
+   void Patrol()
+   {
+      if (route.Length == 0)
+         return;
+
+      transform.position = Vector2.MoveTowards(transform.position, route[routeDest].position, speed * Time.deltaTime);
+
+      routeDest = (routeDest + 1) % route.Length;
+   }
 }
