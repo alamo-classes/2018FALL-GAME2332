992622223f9dc957bec666b87c049869de0cb455
SAKillen <skillen3@alamo.edu>
2018-10-02 19:55:13 -0500

    Branch cleanup
    Deleted and remade branch to circumvent a grievous merge conflict.

--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/PlayerHealth.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/PlayerHealth.cs
@@ -2,30 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
 
-	public float health;
+   public float health;
+   bool isVulnerable = true;
+   float timer;
+   public float invulnerableTime;
 
-	public void addHealth(float heal = 1)
-	{
-		health += heal;
-	}
+   public void Update()
+   {
+      timer += Time.deltaTime;
+      if (timer > invulnerableTime)
+         isVulnerable = true;
+      else
+         isVulnerable = false;
+   }
 
-	public void loseHealth(float damage = 1)
-	{
-		health -= damage;
-	}
+   public void addHealth(float heal = 1)
+   {
+      health += heal;
+   }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        GameObject obj = other.gameObject;
-        HealthOrb healingOrb = other.GetComponent<HealthOrb>();
-        if (healingOrb)
-        {
-            addHealth(healingOrb.HealingPoints());
-            Destroy(other.gameObject);
-        }
-    }
+   public void loseHealth(float damage = 1)
+   {
+      health -= damage;
+   }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      GameObject obj = other.gameObject;
+      HealthOrb healingOrb = other.GetComponent<HealthOrb>();
+      EnemyAttack attack = other.GetComponent<EnemyAttack>();
+
+      if (healingOrb)
+      {
+         addHealth(healingOrb.HealingPoints());
+         Destroy(other.gameObject);
+      }
+
+      if (attack && isVulnerable)
+      {
+         loseHealth(attack.attackDamage);
+         timer = 0f;
+      }
+   }
 
 
 }
