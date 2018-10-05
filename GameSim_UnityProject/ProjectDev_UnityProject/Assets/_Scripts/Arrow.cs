be8858d9e62d6e5f56b6550835a1a47d998e2c60
egatlin1 <egatlin1@student.alamo.edu>
2018-10-05 11:29:59 -0500

    Added first draft Bow and Arrow

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Arrow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+   public float speed = 1f;
+   public float damage = 1f;
+
+
+   private Vector2 trajectory;
+
+   // Use this for initialization
+   void Awake ( )
+   {
+       trajectory = new Vector2(speed, 0f);
+   }
+
+   // Update is called once per frame
+   void Update ( )
+   { 
+      this.transform.Translate(trajectory * Time.deltaTime);
+   }
+
+   void OnTriggerEnter2D ( Collider2D other )
+   {
+      Debug.Log(other.name);
+      if (other.tag == "Enemy")
+      {
+         other.gameObject.GetComponent<EnemyHealth>().RemoveHealth(damage);
+         Debug.Log("hit Enemy");
+      }
+   }
+}
---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Bow.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : Item
+{
+   public GameObject arrow;
+   public float fireRate = 1f;
+
+   private PlayerController player;
+   private float timer = 0f;
+
+   private void Awake ( )
+   {
+      player = GetComponent<PlayerController>();
+   }
+
+   public override void Use ( )
+   {
+      if ( timer >= fireRate)
+      {
+         Shoot();
+         timer = 0f;
+      }
+   }
+
+   private void Update ( )
+   {
+      timer += Time.deltaTime;
+      if (Input.GetKeyDown(KeyCode.P))
+         Use();
+   }
+
+   private void Shoot ( )
+   {
+      Vector3 dir = new Vector3();
+
+
+      switch ( player.dir )
+      {
+         case PlayerController.Direction.Up :
+            dir.z = 90f;
+            break;
+         case PlayerController.Direction.Left :
+            dir.z = 180f;
+            break;
+         case PlayerController.Direction.Down :
+            dir.z = -90f;
+            break;
+         case PlayerController.Direction.Right :
+            dir.z = 0f;
+            break;
+      }
+
+      GameObject bolt = Instantiate(arrow, this.transform.position, Quaternion.identity) as GameObject;
+      bolt.transform.rotation = Quaternion.Euler(dir);
+   }
+}
