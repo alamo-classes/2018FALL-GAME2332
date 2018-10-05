822e021bb1436268337bfaa07f2f9c7daad2a3a0
egatlin1 <egatlin1@student.alamo.edu>
2018-10-05 11:52:09 -0500

    Arrrows now destroy themselves after a given lifeSpan

--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Arrow.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Arrow.cs
@@ -6,14 +6,15 @@ public class Arrow : MonoBehaviour
 {
    public float speed = 1f;
    public float damage = 1f;
-
+   public float lifeSpan = 2f;
 
    private Vector2 trajectory;
 
    // Use this for initialization
    void Awake ( )
    {
-       trajectory = new Vector2(speed, 0f);
+      trajectory = new Vector2(speed, 0f);
+      Invoke("SelfDestruct", lifeSpan);
    }
 
    // Update is called once per frame
@@ -22,6 +23,11 @@ public class Arrow : MonoBehaviour
       this.transform.Translate(trajectory * Time.deltaTime);
    }
 
+
+   private void SelfDestruct ( )
+   {
+      Destroy(gameObject);
+   }
    void OnTriggerEnter2D ( Collider2D other )
    {
       Debug.Log(other.name);
@@ -29,7 +35,7 @@ public class Arrow : MonoBehaviour
       {
          other.gameObject.GetComponent<EnemyHealth>().RemoveHealth(damage);
          Debug.Log("hit Enemy");
-         Destroy(gameObject);
+         SelfDestruct();
       }
    }
 }
