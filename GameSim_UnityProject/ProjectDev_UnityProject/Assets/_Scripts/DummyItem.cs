41481c5c0ba15097845db9dcfd5f68210d4a05e2
RickMP <rpena280@student.alamo.edu>
2018-10-09 19:45:39 -0500

    Fixed error with inventory
    Added Inventory script to player prefab
    Added DummyItem prefab
    Added debug statements to inventory script
    Renamed InventoryTestScene to RicksTesting
    Added Arrow connection to the bow - E

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/DummyItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyItem : Item {
+
+	public override void Use()
+	{
+		Debug.Log("Testing Key Items");
+	}
+	
+}
