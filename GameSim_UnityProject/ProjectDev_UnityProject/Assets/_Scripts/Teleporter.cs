527edef2579897ff876fd84a9816f9c38a12e447
SAKillen <skillen3@alamo.edu>
2018-10-11 19:54:06 -0500

    Teleporter created

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : Item
+{
+   public GameObject destination;
+   GameObject player;
+
+   private void Awake()
+   {
+      player = GameObject.FindGameObjectWithTag("Player");
+   }
+
+   public override void Use()
+   {
+      //Play teleport animation
+      //Should it be interruptible?
+      player.transform.position = destination.transform.position;
+   }
+}
