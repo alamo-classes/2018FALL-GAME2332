838a3fd11394f0add886ba859a9a4bf5b0a11f35
egatlin1 <egatlin1@student.alamo.edu>
2018-10-13 12:22:30 -0500

    Enemy movements fully working
    The ArcherEnemy now has a detection range that will move the player closer, and once it is in firing range it lines up the shot .

--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/EnemyArcher.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/EnemyArcher.cs
@@ -24,10 +24,14 @@ public class EnemyArcher : MonoBehaviour
    void Update ( )
    {
       float detectDistance = (player.transform.position - transform.position).sqrMagnitude;
-      
-      if ( detectDistance <= detectRange)
+
+      if (detectDistance <= detectRange)
       {
          moveCloser = true;
+      }
+      else
+      {
+         moveCloser = false;
       }
 
       if ( detectDistance <= attackRange)
@@ -35,12 +39,49 @@ public class EnemyArcher : MonoBehaviour
          inRange = true;
          moveCloser = false;
       }
+      else
+      {
+         inRange = false;
+      }
 
       if ( moveCloser)
       {
          transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
       }
+      else if ( inRange )
+      {
+         LineUpShot();
+      }
    }
 
+   public void LineUpShot ( )
+   {
+      float xDist = player.transform.position.x - transform.position.x;
+      float yDist = player.transform.position.y - transform.position.y;
+
+      if (transform.position.y > player.transform.position.y)
+      {
+         yDist *= -1;
+      }
+      if (transform.position.x > player.transform.position.x)
+      {
+         xDist *= -1;
+      }
+
+      if ( xDist < yDist)
+      {
+         transform.position = Vector2.MoveTowards( transform.position, 
+                                                   new Vector2(player.transform.position.x, transform.position.y), 
+                                                   speed * Time.deltaTime);
+      }
+      else
+      {
+         transform.position = Vector2.MoveTowards(transform.position,
+                                          new Vector2(transform.position.x, player.transform.position.y),
+                                          speed * Time.deltaTime);
+      }
+
+
+   }
 
 }
