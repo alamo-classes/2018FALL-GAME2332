e14524b74270ebfdd93179b055324dd9987e68a2
SAKillen <skillen3@alamo.edu>
2018-10-16 19:58:19 -0500

    Added damage checks

--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Teleporter.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Teleporter.cs
@@ -8,15 +8,19 @@ public class Teleporter : Item
    public new GameObject camera;
    public GameObject cameraDestination;
    GameObject player;
+   PlayerHealth playerHealth;
 
    private void Awake()
    {
       player = GameObject.FindGameObjectWithTag("Player");
+      playerHealth = player.GetComponent<PlayerHealth>();
+
    }
 
    public override void Use()
    {
       //Play teleport animation
+      //Should it wind up?
       //Should it be interruptible?
       player.transform.position = destination.transform.position;
       camera.transform.position = cameraDestination.transform.position;
