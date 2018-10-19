621d81a5b80ea7dc80348f40b451506fec731af3
egatlin1 <egatlin1@student.alamo.edu>
2018-10-18 20:08:23 -0500

    Hidden areas can now be revealed
    Objects in the world that will eventually need to be revealed need to use the FakeObject.sc, which registers that object to the FakeObjectRegistry
    
    When the player collects the Reveal Hidden Objects Item the registry deactivates all it's collection of "Fake" objects

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/FakeObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeObject : MonoBehaviour
+{
+   void Awake ( )
+   {
+      GameObject.FindGameObjectWithTag("FakeObjectRegistry").GetComponent<FakeObjectsRegistry>().RegisterFakeObject(this.gameObject);
+   }
+
+}
---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/RevealAreas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealAreas : MonoBehaviour
+{
+   FakeObjectsRegistry registry;
+   // Use this for initialization
+   void Awake ( )
+   {
+      registry = GameObject.FindGameObjectWithTag("FakeObjectRegistry").GetComponent<FakeObjectsRegistry>();
+   }
+
+
+
+   private void OnTriggerEnter2D ( Collider2D other )
+   {
+      if ( other.tag == "Player")
+      {
+         registry.HideAllFakeObjects();
+      }
+   }
+}
