f8ec6496fa98d18d2f351c5059996e91d3663822
egatlin1 <egatlin1@student.alamo.edu>
2018-10-25 18:07:19 -0500

    Fixed all the things
    Made an enemy swordsman
    items now move out of screen when picked up

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/EnemySwordsman.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySwordsman : MonoBehaviour
+{
+   public float speed = 2f;
+   public float detectRange = 30;
+   public float swordAttackRange = 5f;
+   bool moveCloser;
+   bool inSwordRange;
+   Transform player;
+   Rigidbody2D m_rigidBody;
+   Direction direction;
+   Sword sword;
+
+   // Use this for initialization
+   void Awake ( )
+   {
+      direction = GetComponent<Direction>();
+      player = GameObject.FindGameObjectWithTag("Player").transform;
+      m_rigidBody = GetComponent<Rigidbody2D>();
+      sword = GetComponentInChildren<Sword>();
+   }
+
+   // Update is called once per frame
+   void FixedUpdate ( )
+   {
+      float detectDistance = (player.transform.position - transform.position).sqrMagnitude;
+      DeterminAction(detectDistance);
+
+      if (moveCloser)
+      {
+         m_rigidBody.MovePosition(Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime));
+      }
+      else if (inSwordRange)
+      {
+         if (detectDistance > 1f)
+            m_rigidBody.MovePosition(Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime));
+         sword.Attack();
+      }
+   }
+
+   public void DeterminAction ( float detectDistance )
+   {
+      if (detectDistance <= detectRange)
+      {
+         moveCloser = true;
+         SetDirection();
+      }
+      else
+      {
+         moveCloser = false;
+      }
+      if (detectDistance <= swordAttackRange)
+      {
+         inSwordRange = true;
+         moveCloser = false;
+      }
+      else
+      {
+         inSwordRange = false;
+      }
+   }
+
+   private void SetDirection ( )
+   {
+      if (player.position.y > transform.position.y)
+         direction.SetFacing(Direction.Facing.Up);
+      else
+         direction.SetFacing(Direction.Facing.Down);
+
+      if (Mathf.Abs(player.position.y - transform.position.y) < Mathf.Abs(player.position.x - transform.position.x))
+      {
+         if (player.position.x > transform.position.x)
+            direction.SetFacing(Direction.Facing.Right);
+         else
+            direction.SetFacing(Direction.Facing.Left);
+      }
+
+   }
+}
