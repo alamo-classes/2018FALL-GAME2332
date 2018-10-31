44773b4349a2110758a027b8634cf9e43eb571a0
egatlin1 <egatlin1@student.alamo.edu>
2018-10-30 19:06:12 -0500

    Added a SceneController

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+   // Singleton
+   void Awake ( )
+   {
+      int numOfMusicControllers = FindObjectsOfType<SceneController>().Length;
+
+      if (numOfMusicControllers > 1)
+         Destroy(gameObject);
+      else
+         DontDestroyOnLoad(this.gameObject);
+
+   }
+
+   public void LoadLevel ( string levelName )
+   {
+      SceneManager.LoadScene(levelName);
+   }
+
+   public void LoadLevel ( int levelIndex )
+   {
+      SceneManager.LoadScene(levelIndex);
+   }
+
+   public void ReloadThisLevel ( )
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+   }
+
+}
