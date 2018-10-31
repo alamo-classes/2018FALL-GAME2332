27ea4c1711518f0d87e8d6e55efbd0dfdc0f2b6d
SAKillen <skillen3@alamo.edu>
2018-10-30 20:02:14 -0500

    Reorganized sound folders

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   public AudioClip villageTheme;
+   public AudioClip inCombatTheme;
+   public AudioClip outOfCombatTheme;
+}
