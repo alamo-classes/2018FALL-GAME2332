81236f115c4a520f5b269ab70ff5a9daeac93fac
SAKillen <skillen3@alamo.edu>
2018-11-01 17:27:51 -0500

    Further work on music transitions

--- GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/AudioManager.cs
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/AudioManager.cs
@@ -7,4 +7,27 @@ public class AudioManager : MonoBehaviour
    public AudioClip villageTheme;
    public AudioClip inCombatTheme;
    public AudioClip outOfCombatTheme;
+   AudioSource backgroundMusic;
+
+   void Start()
+   {
+      backgroundMusic = GetComponent<AudioSource>();
+      backgroundMusic.clip = villageTheme;
+      backgroundMusic.Play();
+   }
+
+   void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.CompareTag("OpenWorld") && backgroundMusic.clip != outOfCombatTheme)
+      {
+         backgroundMusic.clip = outOfCombatTheme;
+         backgroundMusic.Play();
+      }
+
+      if (other.CompareTag("Village") && backgroundMusic.clip != villageTheme)
+      {
+         backgroundMusic.clip = villageTheme;
+         backgroundMusic.Play();
+      }
+   }
 }
