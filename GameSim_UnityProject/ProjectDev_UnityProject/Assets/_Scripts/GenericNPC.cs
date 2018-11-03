786f449c37d644cb06c0c08df4a218d584e457a6
egatlin1 <egatlin1@student.alamo.edu>
2018-11-03 10:50:58 -0500

    Added Generic NPC
    The NPC has a circle collider around it and when the player enters that area a random line of dialogue pops up. When the player leaves that area the text disappears.

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/GenericNPC.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenericNPC : MonoBehaviour
+{
+   [SerializeField] Text text;
+   [SerializeField] string[] dialogue;
+
+   // Use this for initialization
+   void Start ( )
+   {
+      text.text = "";
+   }
+
+   // Update is called once per frame
+   void Update ( )
+   {
+
+   }
+
+
+   private void OnTriggerEnter2D ( Collider2D other )
+   {
+      if ( other.tag == "Player" )
+      {
+         int rand = Random.Range(0, dialogue.Length);
+         text.text = dialogue[rand];
+      }
+   }
+
+   private void OnTriggerExit2D ( Collider2D other )
+   {
+      if ( other.tag == "Player" )
+      {
+         text.text = "";
+      }
+   }
+}
