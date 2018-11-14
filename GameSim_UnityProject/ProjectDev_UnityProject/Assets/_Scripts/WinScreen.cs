036c5fad8c55bcb0350547f7cb511caa7f4c28b9
RickMP <rpena280@student.alamo.edu>
2018-11-13 19:34:23 -0600

    Added New Scene, Added WinScreen script, edited ProgressManager script, added winscreen to UI prefab

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/WinScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour {
+
+	public ProgressManager winner;
+	public Text winText;
+
+	void Update ()
+	{
+		if(winner.hasWon())
+		{
+			winText.gameObject.SetActive(true);
+		}
+	}
+}
