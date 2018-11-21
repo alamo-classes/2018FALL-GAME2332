61bd14b45f9e8023c197b822aabfe7d433349e04
egatlin1 <egatlin1@student.alamo.edu>
2018-11-20 19:24:04 -0600

    Added Campfire prefab
    Added the Campfire prefab that heals the player over time if the player is in range

---
+++ GameSim_UnityProject/ProjectDev_UnityProject/Assets/_Scripts/Campfire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : MonoBehaviour
+{
+   PlayerHealth player;
+
+
+   private void OnTriggerEnter2D ( Collider2D other )
+   {
+      if (other.tag == "Player")
+      {
+         player = other.GetComponent<PlayerHealth>();
+         StartCoroutine("HealPlayer");
+      }
+   }
+
+
+   private void OnTriggerExit2D ( Collider2D other )
+   {
+      if (other.tag == "Player")
+         StopCoroutine("HealPlayer");
+   }
+
+   IEnumerator HealPlayer ( )
+   {
+      while ( true )
+      {
+         player.AddHealth();
+         yield return new WaitForSeconds(2f);
+      }
+   }
+
+}
